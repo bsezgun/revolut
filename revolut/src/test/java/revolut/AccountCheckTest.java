8f8bd6d825b7c51d3e2d8d1f04d192e6096b3fb4
SONY <SONY@bahadir>
2018-09-17 21:44:50 +0300

    Add Test Framework

---
+++ revolut/src/test/java/revolut/AccountCheckTest.java
@@ -0,0 +1,21 @@
+package revolut;
+
+import org.junit.Test;
+
+import com.revolut.money.transfer.entity.Account;
+
+import static org.junit.Assert.*;
+
+public class AccountCheckTest {
+
+	 @Test
+	 public void testConcatenate() {
+	       String result="onetwo";
+	       
+	       Account account=new Account();
+	       
+	        assertNotNull(account);
+
+	 }
+	
+}
